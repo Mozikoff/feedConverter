ff353ec9f821e4a1bcae243fa85e43227d436aa8
emozikov <emozikov@ozon.ru>
2020-05-05 17:01:12 +0300

    version 1.0

--- src/main.py
+++ src/main.py
@@ -1,27 +1,72 @@
 from xml.etree import ElementTree
 import csv
+import requests
+import sys
+import time
 
-def fromXmlToCsv():
-    tree = ElementTree.parse('C:\\Users\\emozikov\\PycharmProjects\\feedConverter\\yandex_730539_k50-2-win1251-series_09_18.php.xml')
-    f = open('test.csv', 'w', newline='', encoding='cp1251')
+
+def from_xml_to_csv(url):
+    root = ElementTree.fromstring(requests.get(url).content)
+    file_name = url.rsplit('/', 1)[1].split('.')[0] + str(time.time()) + '.csv'
+    f = open(file_name, 'w', newline='', encoding='cp1251')
     csvwriter = csv.writer(f, delimiter=';')
 
-    col_names = ['offer_id', 'url', 'price', 'categoryId', 'picture', 'name']
+    col_names = ['id', 'group id', 'available', 'typePrefix', 'vendor', 'model', 'name', 'category', 'price',
+                 'oldprice', 'currencyId', 'url', 'vendorcode', 'picture', 'description', 'country_of_origin',
+                 'manufacturer_warranty', 'sales_notes', 'pickup', 'store', 'delivery', 'barcode', 'adult']
     csvwriter.writerow(col_names)
 
-    root = tree.getroot()
     for offer in root.iter('offer'):
         data = []
-        data.append(offer.attrib['id'])
-        data.append(offer.find('url').text)
-        data.append(offer.find('price').text)
-        data.append(offer.find('categoryId').text)
-        data.append(offer.find('picture').text)
-        data.append(offer.find('name').text)
+        data.append(offer.attrib.get('id'))
+        data.append(offer.attrib.get('group_id'))
+        data.append(offer.attrib.get('available', 'true'))
+        data.append(offer.findtext('typePrefix'))
+        data.append(offer.findtext('vendor'))
+        data.append(offer.findtext('model'))
+        data.append(offer.findtext('name'))
+        category_path = get_category_path(root, offer.findtext('categoryId'))
+        data.append(category_path)
+        data.append(offer.findtext('price'))
+        data.append(offer.findtext('oldprice'))
+        data.append(offer.findtext('currencyId'))
+        data.append(offer.findtext('url'))
+        data.append(offer.findtext('vendorCode'))
+        data.append(offer.findtext('picture'))    #TODO: many
+        data.append(offer.findtext('description'))
+        data.append(offer.findtext('country_of_origin'))
+        data.append(offer.findtext('manufacturer_warranty'))
+        data.append(offer.findtext('sales_notes'))
+        data.append(offer.findtext('pickup'))
+        data.append(offer.findtext('store'))
+        data.append(offer.findtext('delivery'))    #TODO: options
+        data.append(offer.findtext('barcode'))
+        data.append(offer.findtext('adult'))
+        data.append(offer.findtext('adult'))
+        #TODO: params
+
         csvwriter.writerow(data)
 
     f.close()
 
 
+def get_category_path(root, category):
+    cat = root.find(f'.//category[@id="{category}"]')
+    path = f'(id:{cat.attrib["id"]}){cat.text}'
+    parent = cat.attrib.get('parentId')
+    while parent:
+        cat = root.find(f'.//category[@id="{parent}"]')
+        path = f'(id:{cat.attrib["id"]}){cat.text}|' + path
+        parent = cat.attrib.get('parentId')
+    return path
+
+
+def main():
+    url = sys.argv[1]
+    print(url)
+    from_xml_to_csv(url)
+    print('Completed!')
+
+
 if __name__ == '__main__':
-    fromXmlToCsv()
+    main()
